5ccd5c454d00040887ccc5e7f0dacc4e8f66b939
priyankagargc27 <40770470+priyankagargc27@users.noreply.github.com>
2018-07-12 14:38:05 -0500

    changes

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
-let number = [];
-let count =0;
-for(let i=0; i<=number; i++)
-{
-count+=1;
-number.push(count);
-}
-console.log(number)
+//let number = [];
+//let count =0;
+//for(let i=0; i<=100; i++)
+//{
+//count+=1;
+//number.push(count);
+//}
+//console.log(number)
 
-for (number=0;number<=number.length;number++)
-{
-    if(number[i]/5===0)
-    {
+for (let i = 0; i < 100; i++) {
+    let currentNumber = i;
+    if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+        console.log("You are monkey and chicken")
+    }
+    else if (currentNumber % 5 === 0) {
         console.log("you are monkey");
     }
-    
-        elseif(number[i]/7===0)
-        {
-            console.log("you are chicken");
-        }
-elseif{number[i]/5===0 && number[i]/7===0)
-{
-    console.log("You are monkey and chicken")
-}
+
+    else if (currentNumber % 7 === 0) {
+        console.log("you are chicken");
+    }
+
+    else {
+        console.log(currentNumber);
+    }
 
 }
