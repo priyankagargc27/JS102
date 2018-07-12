31ea74c0000514de806cd5990d6304694a597e90
priyankagargc27 <40770470+priyankagargc27@users.noreply.github.com>
2018-07-12 11:57:29 -0500

    js code added

---
+++ main.js
@@ -0,0 +1,26 @@
+let number = [];
+let count =0;
+for(let i=0; i<=number; i++)
+{
+count+=1;
+number.push(count);
+}
+console.log(number)
+
+for (number=0;number<=number.length;number++)
+{
+    if(number[i]/5===0)
+    {
+        console.log("you are monkey");
+    }
+    
+        elseif(number[i]/7===0)
+        {
+            console.log("you are chicken");
+        }
+elseif{number[i]/5===0 && number[i]/7===0)
+{
+    console.log("You are monkey and chicken")
+}
+
+}
